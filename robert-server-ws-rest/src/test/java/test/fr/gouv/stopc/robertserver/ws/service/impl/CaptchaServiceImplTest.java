a7b8ee5d8dc69f714131cf90794102dbfd223ec7
Julia StopCovid <10500-julia-stopcovid@users.noreply.gitlab.inria.fr>
2020-05-14 17:01:12 +0200

    fix: Integer vs Long comparison

--- robert-server-ws-rest/src/test/java/test/fr/gouv/stopc/robertserver/ws/service/impl/CaptchaServiceImplTest.java
+++ robert-server-ws-rest/src/test/java/test/fr/gouv/stopc/robertserver/ws/service/impl/CaptchaServiceImplTest.java
@@ -1,21 +1,28 @@
 package test.fr.gouv.stopc.robertserver.ws.service.impl;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-import fr.gouv.stopc.robertserver.ws.service.impl.CaptchaServiceImpl;
+import java.util.Date;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.web.client.RestTemplate;
 
 import fr.gouv.stopc.robert.server.common.service.IServerConfigurationService;
+import fr.gouv.stopc.robertserver.ws.dto.CaptchaDto;
+import fr.gouv.stopc.robertserver.ws.service.impl.CaptchaServiceImpl;
 import fr.gouv.stopc.robertserver.ws.vo.RegisterVo;
-
 
 @ExtendWith(SpringExtension.class)
 public class CaptchaServiceImplTest {
+
+	private static final String HOSTNAME = "fr.gouv.stopc";
 
 	@InjectMocks
 	private CaptchaServiceImpl captchaServiceImpl;
@@ -30,7 +37,7 @@ public class CaptchaServiceImplTest {
 	public void testVerifyCaptchaWhenVoIsNull() {
 
 		// When
-		boolean isVerified = captchaServiceImpl.verifyCaptcha(null);
+		boolean isVerified = this.captchaServiceImpl.verifyCaptcha(null);
 
 		// Then
 		assertTrue(isVerified);
@@ -39,8 +46,15 @@ public class CaptchaServiceImplTest {
 	@Test
 	public void testVerifyCaptchaWhenVoIsNotNull() {
 
+		// Given
+		CaptchaDto captchaDto = CaptchaDto.builder().success(true).challengeTimestamp(new Date()).appPackageName(HOSTNAME).build();
+		when(this.restTemplate.postForEntity(any(String.class), any(), any())).thenReturn(ResponseEntity.ok(captchaDto));
+
+		when(this.serverConfigurationService.getCaptchaAppPackageName()).thenReturn(HOSTNAME);
+		when(this.serverConfigurationService.getCaptchaChallengeTimestampTolerance()).thenReturn(3600);
+
 		// When
-		boolean isVerified = captchaServiceImpl.verifyCaptcha(RegisterVo.builder().build());
+		boolean isVerified = this.captchaServiceImpl.verifyCaptcha(RegisterVo.builder().build());
 
 		// Then
 		assertTrue(isVerified);
