101854d9ff53b01bc42c42e1be2fdd57e38a8851
Deniro StopCovid <10502-deniro-stopcovid@users.noreply.gitlab.inria.fr>
2020-05-17 14:31:08 +0000

    feat: Update the test of KeyServiceImplTest

--- robert-crypto-grpc-server/src/test/java/test/fr/gouv/stopc/robert/crypto/grpc/server/service/impl/KeyServiceImplTest.java
+++ robert-crypto-grpc-server/src/test/java/test/fr/gouv/stopc/robert/crypto/grpc/server/service/impl/KeyServiceImplTest.java
@@ -10,22 +10,26 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import fr.gouv.stopc.robert.crypto.grpc.server.model.ECDHKeys;
+import fr.gouv.stopc.robert.crypto.grpc.server.service.IKeyService;
 import fr.gouv.stopc.robert.crypto.grpc.server.service.impl.KeyServiceImpl;
 import fr.gouv.stopc.robert.server.common.utils.ByteUtils;
+import fr.gouv.stopc.robert.server.crypto.service.CryptoService;
+import fr.gouv.stopc.robert.server.crypto.service.impl.CryptoServiceImpl;
 
 @ExtendWith(SpringExtension.class)
 public class KeyServiceImplTest {
     
-    @InjectMocks
-    KeyServiceImpl generationKeyService;
+    
+    IKeyService keyService = new KeyServiceImpl();
+    
+    CryptoService cryptoService = new CryptoServiceImpl();
 
     @BeforeEach
     public void beforeEach() {
-        assertNotNull(generationKeyService);
+        assertNotNull(keyService);
     }
 
     @Test
@@ -35,7 +39,7 @@ public class KeyServiceImplTest {
         byte [] clientPublicKey = null;
  
         // When
-        Optional<ECDHKeys> keys = this.generationKeyService.generateECHKeysForEncryption(clientPublicKey);
+        Optional<ECDHKeys> keys = this.keyService.generateECHKeysForEncryption(clientPublicKey);
 
         // Then
         assertFalse(keys.isPresent());
@@ -49,7 +53,7 @@ public class KeyServiceImplTest {
         byte [] clientPublicKey = ByteUtils.generate(50);
  
         // When
-        Optional<ECDHKeys> keys = this.generationKeyService.generateECHKeysForEncryption(clientPublicKey);
+        Optional<ECDHKeys> keys = this.keyService.generateECHKeysForEncryption(clientPublicKey);
 
         // Then
         assertFalse(keys.isPresent());
@@ -60,13 +64,10 @@ public class KeyServiceImplTest {
     public void testGenerateECHKeysForEncryptionWhenClientPublicKeyHasRightSize() {
 
         // Given
-        byte [] clientPublicKey = new byte [] { 48, 89, 48, 19, 6, 7, 42, -122, 72, -50, 61, 2, 1, 6, 8, 42, -122, 72, -50, 61, 3, 1, 7, 3, 66, 0, 
-                                  4, -61, -16, -102, -1, 37, -72, 88, 17, -6, 19, 79, 57, 68, -93, 26, 102, 6, -59, -93, -79, -100, 123, -101,
-                                  -113, -14, 87, 21, -52, -38, -30, 72, -26, -35, 67, -46, -115, 42, -112, 64, 45, -40, 82, 100, 
-                                  115, 0, 80, -51, -30, 9, 29, 105, -103, -95, -33, -101, -111, 127, 22, 21, 71, 50, 91, -35, 11};
+        byte [] clientPublicKey = this.cryptoService.generateECDHPublicKey();
  
         // When
-        Optional<ECDHKeys> keys = this.generationKeyService.generateECHKeysForEncryption(clientPublicKey);
+        Optional<ECDHKeys> keys = this.keyService.generateECHKeysForEncryption(clientPublicKey);
 
         // Then
         assertTrue(keys.isPresent());
