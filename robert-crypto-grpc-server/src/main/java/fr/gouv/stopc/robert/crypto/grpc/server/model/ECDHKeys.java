b4a08e10b43e712442b1b6376b4d8789dee32dd6
Deniro StopCovid <10502-deniro-stopcovid@users.noreply.gitlab.inria.fr>
2020-05-17 13:16:12 +0000

    feat: Add the class ECDHKeys

---
+++ robert-crypto-grpc-server/src/main/java/fr/gouv/stopc/robert/crypto/grpc/server/model/ECDHKeys.java
@@ -0,0 +1,20 @@
+package fr.gouv.stopc.robert.crypto.grpc.server.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class ECDHKeys {
+
+    @ToString.Exclude
+    private byte [] serverPublicKey;
+
+    @ToString.Exclude
+    private byte [] generatedSharedSecret;
+}
