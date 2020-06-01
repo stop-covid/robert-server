c659826ab4689439687e1b1c3b4e23c09f86eee9
Plant StopCovid <10563-plant-stopcovid@users.noreply.gitlab.inria.fr>
2020-06-01 10:42:43 +0200

    Avoid to write null attributes

--- robert-config/src/main/java/fr/gouv/stopc/robert/config/dto/FunctionalConfiguration.java
+++ robert-config/src/main/java/fr/gouv/stopc/robert/config/dto/FunctionalConfiguration.java
@@ -1,10 +1,14 @@
 package fr.gouv.stopc.robert.config.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@JsonInclude(Include.NON_NULL)
 public class FunctionalConfiguration {
 
 	private AccountManagement accountManagement;
