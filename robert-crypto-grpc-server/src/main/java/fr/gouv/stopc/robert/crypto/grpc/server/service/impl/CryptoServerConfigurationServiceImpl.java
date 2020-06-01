9b44aef9bce53209e011b97d6bc29b91a0ca74e1
Plant StopCovid <10563-plant-stopcovid@users.noreply.gitlab.inria.fr>
2020-06-01 17:32:31 +0200

    Initial commit
    Externalization of some conf params + evolution of the timestart
    management

--- robert-crypto-grpc-server/src/main/java/fr/gouv/stopc/robert/crypto/grpc/server/service/impl/CryptoServerConfigurationServiceImpl.java
+++ robert-crypto-grpc-server/src/main/java/fr/gouv/stopc/robert/crypto/grpc/server/service/impl/CryptoServerConfigurationServiceImpl.java
@@ -1,26 +1,47 @@
 package fr.gouv.stopc.robert.crypto.grpc.server.service.impl;
 
-import fr.gouv.stopc.robert.server.common.utils.TimeUtils;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import fr.gouv.stopc.robert.crypto.grpc.server.service.ICryptoServerConfigurationService;
-
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
-
+import fr.gouv.stopc.robert.server.common.utils.TimeUtils;
 
 /**
- * TODO : remove or update this class
- * Implementation of a fake service waiting server repository to be accessible.
+ * Default implementation of the ICryptoServerConfigurationService
  */
 @Service
 public class CryptoServerConfigurationServiceImpl implements ICryptoServerConfigurationService {
 
-    @Override
-    public long getServiceTimeStart() {
-        final LocalDateTime ldt = LocalDateTime.of(2020, 4, 14, 00, 00);
-        final ZonedDateTime zdt = ldt.atZone(ZoneId.of("Europe/Paris"));
-        return TimeUtils.convertUnixMillistoNtpSeconds(zdt.toInstant().toEpochMilli());
-    }
+	@Value("${robert.server.time-start}")
+	private String timeStart;
+
+	@Value("${robert.protocol.hello-message-timestamp-tolerance:180}")
+	private Integer helloMessageTimeStampTolerance;
+	
+	private Long timeStartNtp;
+
+	/**
+	 * Initializes the timeStartNtp field
+	 */
+	@PostConstruct
+	private void initTimeStartNtp() {
+		LocalDate ld = LocalDate.parse(timeStart, DateTimeFormatter.BASIC_ISO_DATE);
+		timeStartNtp = TimeUtils.convertUnixStoNtpSeconds(ld.atStartOfDay().toEpochSecond(ZoneOffset.UTC));
+	}
+	
+	@Override
+	public long getServiceTimeStart() {
+		return this.timeStartNtp;
+	}
+	
+	@Override
+	public int getHelloMessageTimeStampTolerance() {
+		return this.helloMessageTimeStampTolerance;
+	}
 }
